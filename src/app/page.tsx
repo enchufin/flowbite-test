c792b25219451763d6030cba8b44b83cfb47741a
enchufin <lincografic@gmail.com>
2024-06-25 14:07:56 +0200

    inicio fallido

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import {Button, Alert, Card, Dropdown } from "flowbite-react";
+
+export default function Home() {
+  return (
+    
+    <main className="flex h-screen items-center justify-between p-24 w-screen">
+      <Alert color="info">Alert!</Alert>
+      <Card href="#" className="max-w-sm">
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+          </p>
+      </Card>
+      <Button> Clicame</Button>
+      
+    </main>
+    
+    );
+}
+
